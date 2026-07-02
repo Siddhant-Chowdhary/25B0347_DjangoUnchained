5269a9baa21b916719ca2a661590cf6f31093c9a
Siddhant-Chowdhary <sidchow25@gmail.com>
2026-07-02 18:56:40 +0530

    Create url.py

---
+++ bounties/url.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import RegisterView, BountyListCreateView, BountyDetailView
+
+urlpatterns = [
+    path('auth/register/', RegisterView.as_view(), name='auth_register'),
+    path('auth/login/', TokenObtainPairView.as_view(), name='auth_login'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='auth_refresh'),
+    
+    path('bounties/', BountyListCreateView.as_view(), name='bounty_list_create'),
+    path('bounties/<int:pk>/', BountyDetailView.as_view(), name='bounty_detail'),
+]
